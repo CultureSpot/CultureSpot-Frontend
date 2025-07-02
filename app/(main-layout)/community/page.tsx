e0eab59d16dad8cfb88a77b69327b83ca7780bae
kwonboryong <kwonboryong@naver.com>
2025-07-02 16:10:11 +0900

    style: 커뮤니티 페이지 서치바 컴포넌트 추가 및 UI 수정

--- app/(main-layout)/community/page.tsx
+++ app/(main-layout)/community/page.tsx
@@ -2,7 +2,7 @@
 
 import PostListToolbar from '@/components/page/community/PostListToolbar';
 import PostListItem from '@/components/page/community/PostListItem';
-// 정렬 순서 바
+import SearchBar from '@/components/common/SearchBar';
 
 // 임시 유저 데이터
 const User = {
@@ -13,13 +13,38 @@ const User = {
 
 const Page = () => {
   return (
-    <div>
+    <div className='px-10 pt-5'>
       <h3 className='font-paperlogy text-[22px] font-normal'>커뮤니티</h3>
-
-      <div className='my-2 border border-lime-400 p-2'>
+      <div className='flex'>
+        <div className='ml-auto'>
+          <SearchBar variant='community' />
+        </div>
+      </div>
+      <div className='mt-2'>
         <PostListToolbar postCount={0} />
       </div>
-      <div className='my-2 border border-lime-400 p-2'>
+
+      <div>
+        <PostListItem
+          postId={0}
+          title={'게시글 제목임'}
+          content={'내용임'}
+          author={User}
+          hits={0}
+          likeCount={0}
+          commentCount={0}
+          createdAt={'123'}
+        />
+        <PostListItem
+          postId={0}
+          title={'게시글 제목임'}
+          content={'내용임'}
+          author={User}
+          hits={0}
+          likeCount={0}
+          commentCount={0}
+          createdAt={'123'}
+        />
         <PostListItem
           postId={0}
           title={'게시글 제목임'}
